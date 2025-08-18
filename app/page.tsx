0b65eaa7250f8af94afd57ba3e57bbd0a203631b
MohamedAlDeep <mohamedsamir22446688@gmail.com>
2025-08-18 20:34:23 +0300

    user session

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,28 @@
 import Image from "next/image";
 import {SignIn} from '@/app/components/SignInC'
-export default function Home() {
+import { redirect } from "next/navigation";
+import { auth } from '@/app/auth'
+import { log } from "console";
+
+export default async function Home() {
+  const session = await auth()
+  let logged = true
+  if(!session){
+    logged = false
+  }
+  if(logged == false){
+    return (
+      <div>
+        <SignIn/>
+      </div>
+    )
+  }
+  console.log(session?.user)
   return (
     <div >
       <h1 className="text-4xl">TripNote</h1>
-      <p></p>
-      <SignIn/>
+      
+      <img src={session?.user?.image ?? './moch.png'} alt="User Avatar" />
     </div>
   );
 }

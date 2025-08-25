fbaffb40fef18817f20db45fc3d81e318b61dcf0
MohamedAlDeep <mohamedsamir22446688@gmail.com>
2025-08-25 22:09:22 +0300

    fix delete

--- app/api/delete/[id]/route.ts
+++ app/api/delete/[id]/route.ts
@@ -1,8 +1,8 @@
 import { NextResponse } from "next/server";
 import prisma from '@/lib/prisma'
 
-export async function DELETE(request: Request, {params}: {params: { id: string} }){
-    const id = Number(params.id)
-    await prisma.note.delete({where: {id}})
-    return NextResponse.json({success: true})
+export async function DELETE(request: Request, context: { params: { id: string } }) {
+    const id = Number(context.params.id);
+    await prisma.note.delete({ where: { id } });
+    return NextResponse.json({ success: true });
 }

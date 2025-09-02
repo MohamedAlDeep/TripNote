05d51a3492aa2939c1e7ceb67f1cbab7d5e22279
MohamedAlDeep <mohamedsamir22446688@gmail.com>
2025-09-02 23:27:43 +0300

    DELETE API FIX

--- app/api/delete/[id]/route.ts
+++ app/api/delete/[id]/route.ts
@@ -1,7 +1,9 @@
 import { NextResponse } from "next/server";
 import prisma from '@/lib/prisma'
 
-export async function DELETE(request: Request): Promise<Response> {
+export async function DELETE( request: Request,
+  { params }: { params: { id: string } }
+): Promise<Response>  {
   const url = new URL(request.url);
   const id = url.pathname.split('/').pop();
   if (!id) return new Response('Missing id', { status: 400 });

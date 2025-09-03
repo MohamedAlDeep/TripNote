fe7bd5ad55185d8d2683ebe9a0bdcdfa3e6e5aa6
MohamedAlDeep <mohamedsamir22446688@gmail.com>
2025-09-03 15:30:06 +0300

    FIX FINAL 4

--- app/api/update/[id]/route.ts
+++ app/api/update/[id]/route.ts
@@ -1,6 +1,8 @@
 import { NextResponse } from "next/server";
 import prisma from "@/lib/prisma";
 
+export const dynamic = 'force-dynamic';
+
 export async function PUT(request: Request) {
     const url = new URL(request.url);
     const id = url.pathname.split('/').pop();
